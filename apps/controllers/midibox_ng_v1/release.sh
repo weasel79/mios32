29c4c5b3796d82ca75a277ee6e89144a33a10349
Blender <blndr@jrblndr.fritz.box>
2019-05-22 23:49:38 +0200

    Fix release script

--- apps/controllers/midibox_ng_v1/release.sh
+++ apps/controllers/midibox_ng_v1/release.sh
@@ -1,4 +1,6 @@
-# $Id$
+#!/usr/bin/env bash
+
+set -e
 
 if [[ "$1" == "" ]]; then
   echo "SYNTAX: release.sh <release-directory>"
@@ -15,23 +17,21 @@ fi
 ###############################################################################
 echo "Creating $RELEASE_DIR"
 
-mkdir $RELEASE_DIR
-cp README.txt $RELEASE_DIR
+mkdir "$RELEASE_DIR"
+cp README.txt "$RELEASE_DIR"
 cp CHANGELOG.txt $RELEASE_DIR
 cp -r cfg $RELEASE_DIR
 cp -r touchosc $RELEASE_DIR
 find $RELEASE_DIR/cfg -type d | grep .svn$ | xargs rm -rf
 
 ###############################################################################
-configs=( stm32f1 lpc17 stm32f4 )
-for i in "${configs[@]}"; do
-  echo "Building for $i"
-  source ../../../../source_me_${i}
-  make cleanall
-  mkdir -p $RELEASE_DIR/$MIOS32_BOARD
-  make > $RELEASE_DIR/$MIOS32_BOARD/log.txt || exit 1
-  cp project.hex $RELEASE_DIR/$MIOS32_BOARD
-done
+
+source ../../../source_me_MBHP_CORE_STM32F4  
+make cleanall
+mkdir -p $RELEASE_DIR/$MIOS32_BOARD
+make > $RELEASE_DIR/$MIOS32_BOARD/log.txt || exit 1
+cp project.hex $RELEASE_DIR/$MIOS32_BOARD
+
 
 ###############################################################################
 make cleanall
